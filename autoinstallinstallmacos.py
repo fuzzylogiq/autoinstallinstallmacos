88aa682c003403ad43a8a2af855a37fcd701663a
Ben Goodstein <ben.goodstein@it.ox.ac.uk>
2019-10-02 16:25:33 +0100

    moving things around

---
+++ autoinstallinstallmacos.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+autoinstallinstallmacos.py
+
+!!DESCRIPTION GOES HERE!!
+
+Copyright (C) University of Oxford 2019
+    Ben Goodstein <ben.goodstein at it.ox.ac.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# Python 3 compatibility shims
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+import os
+import json
+import datetime
+import plistlib
+
+from installinstallmacos import *
+workdir = "."
+
+def default(o):
+    if isinstance(o, (datetime.date, datetime.datetime)):
+        return o.isoformat()
+
+def check_cache(workdir):
+    cache_path = os.path.join(workdir, ".cached_products")
+    if os.path.isfile(cache_path):
+        with open(cache_path, "r") as f:
+            return plistlib.readPlist(f)
+
+def write_cache(workdir, product_id, product):
+    cache_path = os.path.join(workdir, ".cached_products")
+    products = {}
+    if os.path.isfile(cache_path):
+        with open(cache_path, "r") as f:
+            products = plistlib.readPlist(f)
+    products[product_id] = product
+    with open(cache_path, "w") as f:
+        plistlib.writePlist(products, f)
+
+def main():
+    su_catalog_url = get_default_catalog()
+
+    catalog = download_and_parse_sucatalog(
+        su_catalog_url, workdir)
+    product_info = os_installer_product_info(
+        catalog, workdir)
+
+    print(product_info)
+
+    if not product_info:
+        print("No macOS installer products found in the sucatalog.",
+              file=sys.stderr)
+        exit(-1)
+
+    cached_products = check_cache(workdir)
+    if cached_products:
+        print("yes found cache!")
+        print(cached_products)
+
+    for product_id, product in product_info.items():
+        if cached_products and  product_id in cached_products.keys():
+            print("Product %s is cached..." % product_id)
+        else:
+            print("Product %s is not in the cache, downloading..." % product_id)
+            replicate_product(catalog, product_id, workdir, ignore_cache=True)
+            write_cache(workdir, product_id, product)
+
+if __name__ == "__main__":
+    main()
